fa15b6f000ffd7333db3fa7ffdbbb5cab0a57880
Enzo Bocalon <enzo.bocalon@hotmail.com>
2023-04-14 16:38:43 -0300

    feat: initial input style

--- src/components/Input.ts
+++ src/components/Input.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export const Input = styled.input`
-  
+  padding: 1rem;
+  border: none;
+  outline: none;
+  border-radius: .25rem;
+  background-color: white;
+  color: white;
+  font-size: 1rem;
+  font-family: inherit;
 `;
