82ffac6cadb88233cffa0deafa62ec9afd4ce93d
Enzo Bocalon <enzo.bocalon@hotmail.com>
2023-04-15 15:39:58 -0300

    feat: added mobile version header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,32 @@
 import { Button } from '../Button';
 import * as S from './styles';
 
+import { MdMenu } from 'react-icons/md';
+
 export default function Header() {
 	return (
 		<S.Container>
 			{/* Temporary */}
 			<h1>Blog</h1>
-			<S.Navbar>
-				<S.NavItems>
+			<MdMenu size={20}/>
+			<S.Wrapper>
+				<S.Navbar>
+					<S.NavItems>
             Blog
-				</S.NavItems>
-				<S.NavItems disabled>
+					</S.NavItems>
+					<S.NavItems disabled>
             Teams
-				</S.NavItems>
-			</S.Navbar>
-			<S.HeaderActions>
-				<S.NavItems>
+					</S.NavItems>
+				</S.Navbar>
+				<S.HeaderActions>
+					<S.NavItems>
           Login
-				</S.NavItems>
-				<Button>
+					</S.NavItems>
+					<Button>
           Register
-				</Button>
-			</S.HeaderActions>
+					</Button>
+				</S.HeaderActions>
+			</S.Wrapper>
 		</S.Container>
 	);
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -13,7 +13,40 @@ export const Container = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  position: relative;
 
+  svg {
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    svg {
+      display: block;
+    }
+  }
+
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  @media (max-width: 768px) {
+    width: fit-content;
+    position: absolute;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    right: 25px;
+    background: linear-gradient(45deg,rgba(44,44,46,1) 0%,rgba(46,46,46,1) 100%);
+    top: 60px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    padding: 1rem;
+    border-radius: 1rem;
+    z-index: 100;
+  }
 `;
 
 export const Navbar = styled.nav`
@@ -23,6 +56,11 @@ export const Navbar = styled.nav`
   gap: 1rem;
   margin-inline: 1rem;
   padding: 1rem;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+  }
 `;
 
 export const NavItems = styled.div<INav>`
@@ -53,6 +91,12 @@ export const NavItems = styled.div<INav>`
         transform: scale(1);
       }
     `};
+
+    @media (max-width: 768px) {
+      ::after {
+        transform: scale(0);
+      }
+    }
   }
 `;
 
@@ -60,4 +104,12 @@ export const HeaderActions = styled.div`
   display: flex;
   align-items: center;
   gap: 1rem;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    padding-top: 1rem;
+    border-top: 2px solid ${({theme}) => theme.darkGray};
+  }
+
 `;
