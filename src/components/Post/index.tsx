60f7deafa9ac69119913df55023638ffc5e2a56f
Enzo Bocalon <enzo.bocalon@hotmail.com>
2023-04-16 19:54:44 -0300

    feat: initial post template

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,7 +1,29 @@
 import * as S from './styles';
+import placeholder from '../../assets/newsPlaceholder.jpg';
 
 export default function Post() {
 	return (
-		<p>post</p>
+		<S.Container>
+			<S.HeaderContainer>
+				<S.ImageContainer>
+					<img src={placeholder.src} />
+				</S.ImageContainer>
+				<S.HeaderInfo>
+					<p>Team fulano</p>
+					<p>Published on date</p>
+					<h1>Lorem ipsum dolor, sit amet consectetur adipisicing</h1>
+					<h2>Description</h2>
+				</S.HeaderInfo>
+			</S.HeaderContainer>
+			<S.BodyContainer>
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, porro possimus? Itaque animi vel, amet deserunt laboriosam cupiditate saepe distinctio fuga maxime molestias nobis laudantium? Tempora illo natus minus cumque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, porro possimus? Itaque animi vel, amet deserunt laboriosam cupiditate saepe distinctio fuga maxime molestias nobis laudantium? Tempora illo natus minus cumque.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, porro possimus? Itaque animi vel, amet deserunt laboriosam cupiditate saepe distinctio fuga maxime molestias nobis laudantium? Tempora illo natus minus cumque.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, porro possimus? Itaque animi vel, amet deserunt laboriosam cupiditate saepe distinctio fuga maxime molestias nobis laudantium? Tempora illo natus minus cumque.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, porro possimus? Itaque animi vel, amet deserunt laboriosam cupiditate saepe distinctio fuga maxime molestias nobis laudantium? Tempora illo natus minus cumque.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, porro possimus? Itaque animi vel, amet deserunt laboriosam cupiditate saepe distinctio fuga maxime molestias nobis laudantium? Tempora illo natus minus cumque.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, porro possimus? Itaque animi vel, amet deserunt laboriosam cupiditate saepe distinctio fuga maxime molestias nobis laudantium? Tempora illo natus minus cumque.
+				</p>
+			</S.BodyContainer>
+		</S.Container>
 	);
 }
--- src/components/Post/styles.ts
+++ src/components/Post/styles.ts
@@ -1,1 +1,67 @@
 import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  position: relative;
+
+  img {
+    width: 100%;
+    max-height: 600px;
+    object-fit: cover;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  ::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: calc(100% - 4px);
+      background-color: black;
+      z-index: 90;
+      opacity: .6;
+    }
+`;
+
+export const HeaderInfo = styled.div`
+  position: absolute;
+  z-index: 95;
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  bottom: 25px;
+  left: 0;
+  right: 0;
+  padding: 1rem;
+
+  > p {
+    color: ${({theme}) => theme.lightGray};
+    font-size: 14px;
+    letter-spacing: 1px;
+    margin-bottom: 1rem;
+
+    :first-of-type {
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+  }
+
+  > h2 {
+    margin-top: 1rem;
+    font-size: 1.25rem;
+  }
+`;
+
+export const BodyContainer = styled.div`
+    width: 100%;
+    max-width: 1440px;
+    padding: 1rem;
+    margin: 0 auto;
+`;
+
