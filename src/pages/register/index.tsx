7dd4643fe9e2c3fc9c94b0afca0dc89f85861859
Enzo Bocalon <enzo.bocalon@hotmail.com>
2023-04-16 13:06:57 -0300

    feat: added register page

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,10 @@
+import Form from '@/components/Form';
+import RootLayout from '@/components/layouts/RootLayout';
+
+export default function Register() {
+	return (
+		<RootLayout>
+			<Form />
+		</RootLayout>
+	);
+}
