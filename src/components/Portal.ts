0a9198674036cf5d2e665bfde83615f9859c3456
Enzo Bocalon <enzo.bocalon@hotmail.com>
2023-05-05 12:21:45 -0300

    feat: added React Portal

---
+++ src/components/Portal.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Portal= ({ children }: Props) => {
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+
+		return () => setMounted(false);
+	}, []);
+
+	return mounted
+		? createPortal(children,
+			document.querySelector('#portal') as HTMLElement)
+		: null;
+};
+
+export default Portal;
