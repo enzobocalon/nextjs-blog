87148d62d690ac8406c491f044beb191498d60af
Enzo Bocalon <enzo.bocalon@hotmail.com>
2023-05-11 13:11:00 -0300

    feat: the route is now secured

--- src/pages/posts/create.tsx
+++ src/pages/posts/create.tsx
@@ -1,5 +1,7 @@
 import PostBox from '@/components/PostBox';
 import RootLayout from '@/components/layouts/RootLayout';
+import { GetServerSidePropsContext } from 'next';
+import { getSession } from 'next-auth/react';
 import Head from 'next/head';
 
 export default function CreatePost() {
@@ -13,4 +15,24 @@ export default function CreatePost() {
 			</RootLayout>
 		</>
 	);
+}
+
+export async function getServerSideProps(context:GetServerSidePropsContext) {
+	const session = await getSession(context);
+
+	if (!session) {
+		context.res.writeHead(302, {Location: '/login'});
+		context.res.end();
+		return {
+			props: {}
+		};
+	}
+
+	if (!['ADMIN', 'AUTHOR'].includes(session.role)) {
+		context.res.writeHead(302, {Location: '/'});
+		context.res.end();
+		return {
+			props: {}
+		};
+	}
 }
